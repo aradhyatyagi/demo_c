07345c57fc0e72f782e6ba05a8276eef2065fb56
aradhyatyagi <157880220+aradhyatyagi@users.noreply.github.com>
2024-04-02 08:01:36 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,9 @@
-/******************************************************************************
-Dated:01/04/2024
-Program for swapping values of two variables.
-By-Aradhya Tyagi
-*******************************************************************************/
+
 #include <stdio.h>
 
 int main()
 {
-    int a,b,temp;
-    printf("Enter the value of a=");
-    scanf("%d",&a);
-    printf("Enter the value of b=");
-    scanf("%d",&b);
-    temp=a;
-    a=b;
-    b=temp;
-    printf("\nValue of a after swapping=%d\n",a);
-    printf("Value of b after swapping=%d",b);
-    
+    printf("Hello World");
+
     return 0;
 }
